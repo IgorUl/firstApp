fad6b70ef30ce70ac79ad67ebcd638c46c42cd95
IgorUl <ulyakin.igor@gmail.com>
2020-08-02 17:02:47 +0300

    sort logic classes

--- app/src/main/java/com/example/firstapp/Logic.kt
+++ app/src/main/java/com/example/firstapp/Logic.kt
@@ -1,6 +1,26 @@
 package com.example.firstapp
 
+import android.widget.EditText
+import android.widget.TextView
+
 class Logic {
+
+    private val stringList = mutableListOf<String>()
+    private val sort = Sort()
+
+
+    fun addString(textView: TextView, editText: EditText) {
+
+        val str = editText.text.toString()
+        stringList.add(str)
+        textView.text = stringList.joinToString("\n", "", "")
+
+    }
+
+    fun sort(textView: TextView) {
+        val sortedList = sort.mergeSort(stringList)
+        textView.text = sortedList.joinToString("\n", "", "")
+    }
 
 
 }
--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -3,33 +3,27 @@ package com.example.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val stringList = mutableListOf<String>()
+    private val logic = Logic()
 
-    //delete
-    val sort = Sort()
-    //
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
 
-    fun addString(view: View) {
-
-        val str = editText.text.toString()
-        stringList.add(str)
-        textView.text = stringList.joinToString("\n", "", "")
-        editText.setText("")
-    }
-
-    fun sort(view: View) {
-        val sortedList = sort.mergeSort(stringList)
-        textView.text = sortedList.joinToString("\n", "", "")
+        // почему если передавать it, то приложение крашиться
+        addButton.setOnClickListener {
+            logic.addString(textView, editText)
+            editText.setText("")
+        }
+        sortButton.setOnClickListener {
+            logic.sort(textView)
+        }
     }
 }
